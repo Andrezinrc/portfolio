c8023f9cb0b37da225da5a399f81a9d128b89dcc
André Moreira <andrem.33333@gmail.com>
2026-04-01 02:15:25 -0300

    fix: corrige link vazio no header.js

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -7,7 +7,7 @@ function Header()
         <header className={styles.header}>
             <nav className={styles.navBar}>
                 <ul className={styles.menu}>
-                    <li className={styles.navList}><a href="#" className={styles.navLink}>Home</a></li>
+                    <li className={styles.navList}><a href="#home" className={styles.navLink}>Home</a></li>
                     <li className={styles.navList}><a href="#about" className={styles.navLink}>About</a></li>
                     <li className={styles.navList}><a href="#skills" className={styles.navLink}>Skills</a></li>
                     <li className={styles.navList}><a href="#projects" className={styles.navLink}>Projects</a></li>
